c5bbbc241db0ca07b575d69716bcb9051bf9d6d5
unknown <subhradeepray2017@gmail.com>
2023-04-29 13:34:39 +0530

    Added Dedicated Module for Variables

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
+import variables
+
 def main():
 	print("Python Main Method")
 
-def variables():
-	x = 100
-	y = "ABCD"
-	z = 10.20
-	print(x, type(x))
-	print(y, type(y))
-	print(z, type(z))
-
 if __name__=="__main__":
 	main()
-	variables()
+	variables.start()
---
+++ variables.py
@@ -0,0 +1,7 @@
+def start():
+	x = 100
+	y = "ABCD"
+	z = 15.25
+	print(x, type(x))
+	print(y, type(y))
+	print(z, type(z))
