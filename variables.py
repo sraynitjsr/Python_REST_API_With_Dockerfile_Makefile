f6dec95ea480b5e45230418eab51179ec010403e
unknown <subhradeepray2017@gmail.com>
2023-04-29 13:39:56 +0530

    Changing type of variable after initializing once

--- variables.py
+++ variables.py
@@ -2,6 +2,14 @@ def start():
 	x = 100
 	y = "ABCD"
 	z = 15.25
+	
 	print(x, type(x))
 	print(y, type(y))
 	print(z, type(z))
+
+	dummyVariable = "Dummy"
+	print(dummyVariable, type(dummyVariable))
+
+	dummyVariable = 2525
+	print(dummyVariable, type(dummyVariable))
+
