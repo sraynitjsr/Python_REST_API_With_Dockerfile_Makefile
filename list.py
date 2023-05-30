8f0c713b3a3ccaf43a8fb96f3d29fff0c0c49d38
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-30 23:52:48 +0530

    Negative Index in List and List Accessing in Python

--- list.py
+++ list.py
@@ -2,5 +2,8 @@ def start():
     myList = ["A", "C", "XYZ"]
     print(myList)
 
-    myMixedList = ["Hi", 2020, 5050, "Bye"]
+    myMixedList = ["Hi", 2020, "Laptop", 5050, "Bye"]
     print(myMixedList)
+
+    print(myMixedList[2])
+    print(myMixedList[-2])
