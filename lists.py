0bb6bda29deae49e43393c07683e4eadad1077ce
unknown <subhradeepray2017@gmail.com>
2023-06-13 17:53:54 +0530

    List in Python

--- lists.py
+++ lists.py
@@ -1,9 +1,10 @@
 def start():
-    print('Inside Variables')
+    print('Inside Lists')
     list1 = ['ABCD', 100 , 35.46, 'XYZ']
     list2 = [123, 'MSD']
     print ('Full List', list1)
     print ('First Element in List', list1[0])
+    print ('Last Element in List', list1[-1])
     print ('List Elements From Index 1 to 3', list1[1:3])
     print ('List Elements From Index 2 to Last', list1[2:])
     print ('Print List Twice', list2 * 2)
