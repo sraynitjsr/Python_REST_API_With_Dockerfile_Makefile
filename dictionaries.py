da0a6e2b15a4a5a92535c5c79b524265b58d69c0
unknown <subhradeepray2017@gmail.com>
2023-06-16 14:26:27 +0530

    Dictionary Add, Modify and Delete in Python

--- dictionaries.py
+++ dictionaries.py
@@ -2,3 +2,7 @@ def start():
     print('Inside Dictionaries in Python')
     my_dict = {"A": 100, "B": 500}
     print(my_dict)
+    my_dict["C"] = 1000
+    print('After Adding C', my_dict)
+    my_dict["B"] = 2020
+    print('After Updating Value for B', my_dict)
