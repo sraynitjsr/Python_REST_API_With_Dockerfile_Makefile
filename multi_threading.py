8fdb951900617e069eae5dcb3924f6d6151e9f86
unknown <subhradeepray2017@gmail.com>
2023-06-20 23:35:57 +0530

    Python Multi-Threading

--- multi_threading.py
+++ multi_threading.py
@@ -1,2 +1,21 @@
+import threading
+
+def task():
+    # Code to be executed in the thread
+    print("Thread critical section")
+    
 def start():
     print('Inside Multi Threading Using Python')
+
+    # Create multiple threads
+    threads = []
+    for _ in range(5):
+        t = threading.Thread(target=task)
+        threads.append(t)
+        t.start()
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    print("All threads have completed")
