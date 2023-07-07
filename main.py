26cb1db5d31339e5ba7eb0384f4640ec91d057b6
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-07-08 00:13:21 +0530

    Simple Variables of Type => Int, String, Float

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import variables
+
 def main():
 	print("Inside Python Main Method")
 
 if __name__=="__main__":
 	main()
+	
+	variables.start()
+	print()
---
+++ variables.py
@@ -0,0 +1,6 @@
+def start():
+    print("Inside Variables Module")
+    age    = 999
+    name   = "Tufan"
+    salary = 1.1
+    print ("Simple Variables of Types => Int, String, Float =>", age, salary, name)
