60543a3d85c3865a74abc960f05e812326026ff9
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-07-08 00:18:23 +0530

    Multiple Assignment in One Statement in Python

--- variables.py
+++ variables.py
@@ -1,6 +1,10 @@
 def start():
     print("Inside Variables Module")
+
     age    = 999
     name   = "Tufan"
     salary = 1.1
     print ("Simple Variables of Types => Int, String, Float =>", age, salary, name)
+
+    a = b = c = 123
+    print("Multiple Assignment", "A =>", a, "B =>", b, "C =>", c)
