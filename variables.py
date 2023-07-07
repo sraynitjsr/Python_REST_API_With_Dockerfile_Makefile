48ddbda09dcf4729a488fc9249a071d782913a06
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-07-08 00:20:02 +0530

    Multiple Assignment in One Statement in Python of Different Data Types

--- variables.py
+++ variables.py
@@ -8,3 +8,6 @@ def start():
 
     a = b = c = 123
     print("Multiple Assignment", "A =>", a, "B =>", b, "C =>", c)
+
+    x,y,z = 1,2.3,"Ray2020"
+    print("Multiple Assignment of Different Types in One Statement =>", x, y, z)
