1382021d9b13bba109437f9c6848489e40497fb0
root <subhradeepray2017@gmail.com>
2023-07-12 00:41:27 +0530

    Continue, Break Statements in Python Loops

--- loops.py
+++ loops.py
@@ -10,3 +10,14 @@ def start():
     while temp <= 10:
         print(temp, end=" ")
         temp = temp + 1
+
+    print("\nBreak and Continue Clause in Loops")
+    var = 100
+    while var > 0:
+        print("Hi", end=" ")
+        var = var - 10
+        if var < 25:
+            print("\nBreaking Loop")
+            break
+        else:
+            continue
