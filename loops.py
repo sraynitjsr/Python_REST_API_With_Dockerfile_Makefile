979356d073433cbdd626776fcc6f0c1d9e1abced
root <subhradeepray2017@gmail.com>
2023-07-12 00:30:00 +0530

    While Loop

--- loops.py
+++ loops.py
@@ -1,4 +1,12 @@
 def start():
     print("Inside Loops in Python")
+
+    print("For Loop =>", end=" ")
     for i in range(1, 6):
         print(i, end=" ")
+
+    print("\nWhile Loop =>", end=" ")
+    temp = 1
+    while temp <= 10:
+        print(temp, end=" ")
+        temp = temp + 1
