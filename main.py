89896a8906df26d1306487eefc7668b56316aa7a
root <subhradeepray2017@gmail.com>
2023-07-11 21:44:31 +0530

    PEP 8 E302 Standard Followed

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import variables
 
-def main():
-	print("Inside Python Main Method")
 
-if __name__=="__main__":
-	main()
-	
-	variables.start()
-	print()
+def main():
+    print("Inside Python Main Method")
+
+
+if __name__ == "__main__":
+    main()
+
+    variables.start()
+    print()
--- variables.py
+++ variables.py
@@ -1,16 +1,16 @@
 def start():
     print("Inside Variables Module")
 
-    age    = 999
-    name   = "Tufan"
+    age = 999
+    name = "Tufan"
     salary = 1.1
-    print ("Simple Variables of Types => Int, String, Float =>", age, salary, name)
+    print("Simple Variables of Types => Int, String, Float =>", age, salary, name)
 
     a = b = c = 123
     print("Multiple Assignment", "A =>", a, "B =>", b, "C =>", c)
 
-    x,y,z = 1,2.3,"Ray2020"
+    x, y, z = 1, 2.3, "Ray2020"
     print("Multiple Assignment of Different Types in One Statement =>", x, y, z)
 
     print("Deleting Variables or Their References in Python")
-    del age, name, name, a, b, c, x, y, z
+    del age, name, a, b, c, x, y, z
