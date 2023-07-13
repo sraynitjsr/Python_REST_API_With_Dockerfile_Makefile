23dbcc6a6235311950e3e6d852d795432ad02d4a
unknown <subhradeepray2017@gmail.com>
2023-07-13 18:41:24 +0530

    JSON to Python Object and Python Object to JSON

--- json_handling.py
+++ json_handling.py
@@ -1,5 +1,5 @@
-
 import json
+from collections import namedtuple
 
 def start():
     print("Inside JSON Handling Using Python")
@@ -11,4 +11,10 @@ def start():
 
     my_data_in_json = json.dumps(my_python_data)
 
-    print("JSON Data", my_data_in_json)
+    print("Python to JSON Data =>", my_data_in_json)
+
+    data = '{"name": "Subhradeep Ray", "city": {"name": "Bengaluru", "id": 560045}}'
+
+    x = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    
+    print("JSON to Python Data =>", x.name, x.city.name, x.city.id)
