8792d1d3ff16e85ec64df4af501cf957913d8caf
unknown <subhradeepray2017@gmail.com>
2023-07-15 05:10:53 +0530

    Learning Python From Scratch

--- README.md
+++
@@ -1,2 +0,0 @@
-## Python Programming Basics
-## Not Just AI, ML etc. Instead, Python as Programming Language
--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import loops
-import variables
-import cmd_args
-import json_handling
-
 def main():
-    print("Inside Python Main Method")
-
+    print("Learning Python From Scratch")
 
 if __name__ == "__main__":
     main()
 
-    variables.start()
-    print()
-
-    loops.start()
-    print()
-
-    cmd_args.start()
-    print()
-
-    json_handling.start()
-    print()
