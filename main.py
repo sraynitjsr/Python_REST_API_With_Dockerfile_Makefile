3ae742ebb5d32fb32c19474626abd8092c0c4e91
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-16 17:23:02 +0530

    OOPS  in Python

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Learning Python From Scratch")
+print("Learning Python From Scratch")
 
-if __name__ == "__main__":
-    main()
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def display(self):
+        print(f"My name is {self.name} & I am {self.age} years old.")
 
+person1 = Person("Ray", 30)
+person2 = Person("Joy", 25)
+
+person1.display()
+person2.display()
