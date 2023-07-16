2f39148aee307db470aeef14b593e93e024f239e
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-16 17:29:24 +0530

    Multiple Inheritance in Python

--- main.py
+++ main.py
@@ -37,3 +37,20 @@ cat.speak()
 
 print(dog.name)
 print(cat.name)
+
+class Parent1:
+    def method1(self):
+        print("Parent1 method")
+
+class Parent2:
+    def method2(self):
+        print("Parent2 method")
+
+class Child(Parent1, Parent2):
+    def child_method(self):
+        print("Child method")
+
+child_obj = Child()
+child_obj.method1()
+child_obj.method2()
+child_obj.child_method()
