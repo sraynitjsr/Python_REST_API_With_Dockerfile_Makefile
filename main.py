9169cc3ce42694c88615486a303936c6e42e9bd9
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-16 17:27:34 +0530

    Single Inheritance in Python

--- main.py
+++ main.py
@@ -13,3 +13,27 @@ person2 = Person("Joy", 25)
 
 person1.display()
 person2.display()
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def speak(self):
+        print("The animal makes a sound.")
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+dog = Dog("Buddy")
+cat = Cat("Whiskers")
+
+dog.speak()
+cat.speak()
+
+print(dog.name)
+print(cat.name)
