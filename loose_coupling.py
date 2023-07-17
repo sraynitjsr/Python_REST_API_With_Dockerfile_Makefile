5470a278494489dffd5286a7b4cee0e76c188967
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-07-17 16:29:09 +0530

    Interface in Python Using Abstract Base Classes (ABCs) From abc Module

---
+++ loose_coupling.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+rectangle = Rectangle(3, 5)
+circle = Circle(4)
+
+print(rectangle.area())
+print(rectangle.perimeter())
+print(circle.area())
+print(circle.perimeter())
