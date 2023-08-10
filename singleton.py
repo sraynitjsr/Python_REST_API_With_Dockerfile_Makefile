053d83c535541020d95d64d70c3453a6a875e1c1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-10 23:53:23 +0530

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -1,12 +1,1 @@
-class Singleton:
-    _instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kwargs)
-        return cls._instance
-
-instance1 = Singleton()
-instance2 = Singleton()
-
-print(instance1 is instance2)
