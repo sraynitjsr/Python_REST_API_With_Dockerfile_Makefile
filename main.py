98ac195d6b6e2b49cd6da1d4483a69f0b5dc74f9
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-08-27 22:54:58 +0530

    Command Line Arguments

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World')
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num1', type=int)
+    parser.add_argument('num2', type=int)
+    
+    args = parser.parse_args()
+    result = args.num1 + args.num2
+    print(f"The Sum of {args.num1} and {args.num2} is {result}")
+
+if __name__ == "__main__":
+    main()
