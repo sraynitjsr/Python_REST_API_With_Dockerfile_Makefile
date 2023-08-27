0633c9f1e92fd4d45c4a1390df7173b348c9f09c
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-08-27 22:34:01 +0530

    Hello World in Python

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-class Student:
-	def __init__(self, name, roll):
-		self.name = name
-		self.roll = roll
-
-	def show(self):
-            print('New Student Name is =>', self.name, 'and Roll No. is =>', self.roll)
-
-student = Student("Subhradeep", 100)
-student.show()
+print('Hello World')
