f48a86a8e21e49a305dc87753843e2620182a0e9
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-05 23:27:52 +0530

    OOPS in Python

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import argparse
+# Define a simple class called "Person"
+class Person:
+    # Constructor method to initialize the object with a name and age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    # Method to display information about the person
+    def display_info(self):
+        print(f"Name=> {self.name}, Age=> {self.age}")
+
+# Define a main function for the script
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('num1', type=int)
-    parser.add_argument('num2', type=int)
-    
-    args = parser.parse_args()
-    result = args.num1 + args.num2
-    print(f"The Sum of {args.num1} and {args.num2} is {result}")
+    # Create instances of the Person class
+    person1 = Person("A", 25)
+    person2 = Person("B", 50)
 
+    # Access and display information about the persons
+    person1.display_info()
+    person2.display_info()
+
+# Check if the script is run directly (not imported as a module)
 if __name__ == "__main__":
     main()
