649b1f387e6dba9d0abcbbf560603ecde19fcf90
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-09 20:54:24 +0530

    DELETE A Particular Task

--- main.py
+++ main.py
@@ -41,5 +41,13 @@ def update_task(task_id):
     task['done'] = request.json.get('done', task['done'])
     return jsonify({'task': task})
 
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task is None:
+        return jsonify({'error': 'Task not found'}), 404
+    tasks.remove(task)
+    return jsonify({'result': True})
+
 if __name__ == '__main__':
     app.run(debug=True)
