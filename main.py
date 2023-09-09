bb9c86fa01dd0bcc98da311fd0ff883a96516582
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-09 20:51:36 +0530

    GET Task by ID

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task is None:
+        return jsonify({'error': 'Task not found'}), 404
+    return jsonify({'task': task})
+
 if __name__ == '__main__':
     app.run(debug=True)
