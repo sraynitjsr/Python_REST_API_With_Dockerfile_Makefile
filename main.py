6e40482dac70b32649274316c7e25693740af057
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-09 20:52:41 +0530

    CREATE New Task

--- main.py
+++ main.py
@@ -18,5 +18,17 @@ def get_task(task_id):
         return jsonify({'error': 'Task not found'}), 404
     return jsonify({'task': task})
 
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        return jsonify({'error': 'Invalid request'}), 400
+    task = {
+        'id': len(tasks) + 1,
+        'title': request.json['title'],
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
