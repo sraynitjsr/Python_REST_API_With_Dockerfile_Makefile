f2408b0b817b50f612bddf46c5308924e9ef8f51
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-09 20:59:52 +0530

    Full CRUD Using Flask and MySQL Database

--- main.py
+++ main.py
@@ -1,52 +1,64 @@
 from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-tasks = [
-    {"id": 1, "title": "Task 1", "done": False},
-    {"id": 2, "title": "Task 2", "done": False},
-]
+# Configure MySQL database connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="sraynitjsr",
+    password="Tufan2020",
+    database="task_database"
+)
+cursor = db.cursor()
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    cursor.execute("SELECT * FROM task")
+    tasks = cursor.fetchall()
+    task_list = [{"id": task[0], "title": task[1], "done": task[2]} for task in tasks]
+    return jsonify({'tasks': task_list})
 
 @app.route('/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    task = next((task for task in tasks if task['id'] == task_id), None)
+    cursor.execute("SELECT * FROM task WHERE task_id = %s", (task_id,))
+    task = cursor.fetchone()
     if task is None:
         return jsonify({'error': 'Task not found'}), 404
-    return jsonify({'task': task})
+    return jsonify({'task': {'id': task[0], 'title': task[1], 'done': task[2]}})
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
     if not request.json or 'title' not in request.json:
         return jsonify({'error': 'Invalid request'}), 400
-    task = {
-        'id': len(tasks) + 1,
-        'title': request.json['title'],
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+    title = request.json['title']
+    cursor.execute("INSERT INTO task (title, done) VALUES (%s, %s)", (title, False))
+    db.commit()
+    new_task_id = cursor.lastrowid
+    return jsonify({'task': {'id': new_task_id, 'title': title, 'done': False}}), 201
 
 @app.route('/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
-    task = next((task for task in tasks if task['id'] == task_id), None)
+    cursor.execute("SELECT * FROM task WHERE task_id = %s", (task_id,))
+    task = cursor.fetchone()
     if task is None:
         return jsonify({'error': 'Task not found'}), 404
     if not request.json:
         return jsonify({'error': 'Invalid request'}), 400
-    task['title'] = request.json.get('title', task['title'])
-    task['done'] = request.json.get('done', task['done'])
-    return jsonify({'task': task})
+    title = request.json.get('title', task[1])
+    done = request.json.get('done', task[2])
+    cursor.execute("UPDATE task SET title = %s, done = %s WHERE task_id = %s", (title, done, task_id))
+    db.commit()
+    return jsonify({'task': {'id': task_id, 'title': title, 'done': done}})
 
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-    task = next((task for task in tasks if task['id'] == task_id), None)
+    cursor.execute("SELECT * FROM task WHERE task_id = %s", (task_id,))
+    task = cursor.fetchone()
     if task is None:
         return jsonify({'error': 'Task not found'}), 404
-    tasks.remove(task)
+    cursor.execute("DELETE FROM task WHERE task_id = %s", (task_id,))
+    db.commit()
     return jsonify({'result': True})
 
 if __name__ == '__main__':
