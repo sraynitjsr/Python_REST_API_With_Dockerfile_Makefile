ebb10f3dcfd7c488b6081c3dd694c6660bd0a438
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-12 19:10:30 +0530

    Flask GET Home API

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello, World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/helloWorld', methods=['GET'])
+def hello_world():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    main()
+    app.run('0.0.0.0',port=8080)
